30d880894d2598b7437dc22c444e866aa385bcca
yangxiaoping <2878847069@qq.com>
2024-09-15 17:25:11 +0800

    chore: 接入@prerenderer/webpack-plugin

--- webpack/webpack.ssg.config.js
+++ webpack/webpack.ssg.config.js
@@ -3,8 +3,9 @@ const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.base.config.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const PrerendererWebpackPlugin = require('@prerenderer/webpack-plugin')
 
 const entry = path.resolve(__dirname, '../ssg/app.js')
 
@@ -12,6 +13,8 @@ module.exports = merge(baseConfig, {
   mode: 'production',
   entry,
   output: {
+    // 指定publicPath以保证SSG的静态资源路径正确
+    publicPath: '/',
     path: path.resolve(__dirname, '../dist'),
     filename: 'js/[name].[chunkhash:4].js'
   },
@@ -23,11 +26,12 @@ module.exports = merge(baseConfig, {
     new HtmlWebpackPlugin({
       title: 'PRODUCTION prerender-spa-plugin',
       template: path.resolve(__dirname, '../public/index.html'),
-      // filename: path.resolve(__dirname, 'dist/index.html'),
+      filename: path.resolve(__dirname, '../dist/index.html'),
       favicon: path.resolve(__dirname, '../public/favicon.ico')
     }),
+    // Maybe not work in webpack@5
     // new PrerenderSPAPlugin({
-    //   staticDir: path.join(__dirname, '../dist'),
+    //   staticDir: path.resolve(__dirname, '../dist'),
     //   routes: [ '/', '/about' ],
 
     //   renderer: new Renderer({
@@ -37,6 +41,28 @@ module.exports = merge(baseConfig, {
     //     headless: true,
     //     renderAfterDocumentEvent: 'render-event'
     //   })
-    // })
+    // }),
+    // injectProperty inject not work...
+    new PrerendererWebpackPlugin({
+      // indexPath: path.resolve(__dirname, '../dist'),
+      routes: ['/', '/about'],
+      // renderer: '@prerenderer/renderer-puppeteer',
+      // renderer: '@prerenderer/renderer-jsdom',
+      rendererOptions: {
+        // headless: false,
+        // renderAfterDocumentEvent: 'render-event',
+        injectProperty: '__PRERENDER_INJECTED',
+        inject: {
+          foo: 'bar' // window.__PRERENDER_INJECTED.foo
+        },
+        // Optional - Wait to render until the specified event is dispatched on the document.
+        // eg, with `document.dispatchEvent(new Event('custom-render-trigger'))`
+        renderAfterDocumentEvent: 'custom-render-trigger',
+
+        // Optional - Wait to render until the specified element is detected using `document.querySelector`
+        // renderAfterElementExists: 'my-app-element',
+        timeout: 10000,
+      }
+    })
   ]
 })
